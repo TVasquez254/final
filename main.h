aa37fe2645776348d30cc9b3f680496060f48f08
TVasquez254 <tvasquez@austincc.edu>
2019-07-10 23:23:08 -0500

    amended files

---
+++ main.h
@@ -0,0 +1,15 @@
+/***********************************************************
+Name: Tomas Vasquez 
+Assignment: Final
+Purpose: 
+Notes:   
+***********************************************************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "functions.h"
+#include "linkedList.h"
+
+
+#endif
