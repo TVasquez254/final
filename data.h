e95f1a020e4aa11b22db7e2531ebee76cfd22f9f
TVasquez254 <tvasquez@austincc.edu>
2019-07-15 11:50:36 -0500

    adding data struct

---
+++ data.h
@@ -0,0 +1,20 @@
+/***********************************************************
+Name: Tomas Vasquez 
+Assignment: Final
+Purpose: 
+Notes:  
+***********************************************************/
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+
+using std::string;
+
+
+typedef struct Node {
+    int id;
+    string data; 
+} DataNode;
+
+#endif
