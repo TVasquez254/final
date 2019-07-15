a7b12c8920d9bb3b5a305ae6d4690f54bc99f753
TVasquez254 <tvasquez@austincc.edu>
2019-07-15 11:56:24 -0500

    adding libraries

--- functions.h
+++ functions.h
@@ -4,3 +4,18 @@ Assignment: Final
 Purpose: 
 Notes:  
 ***********************************************************/
+
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <time.h>
+#include "data.h"
+
+using namespace std;
+ 
+
+#endif
