c39814e00a011f67a644a13dccc99fbd6711d0e8
TVasquez254 <tvasquez@austincc.edu>
2019-07-19 11:15:08 -0500

    adding functions to graph.h

--- graph.cpp
+++ graph.cpp
@@ -1,1 +1,16 @@
 #include "graph.h"
+
+
+//Constructor
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+
+// add edge to graph 
+void Graph :: addEdge ( int u, int v ) 
+{ 
+    adj[u].push_back(v); 
+    adj[v].push_back(u); 
+} 
--- graph.h
+++ graph.h
@@ -7,7 +7,26 @@ Notes:
 
 #ifndef GRAPH_H
 #define GRAPH_H
+#include <list>
+#include <vector>
+#include<iostream> 
 
+// Adjacency list representation of graph 
+class Graph 
+{ 
+    int V;                          // No. of vertices 
+    std::list<int> *adj;        // Pointer to an array containing adjacency lists 
+    
+public : 
+    Graph( int V ); 
+    void addEdge ( int u, int v ) ;     // function to add an edge to graph 
+    int countEdges();           // function to count # of edge 
+    int countVertex();          // function to count # of vertecies 
+    bool isConnected();         // function to determine if graph is connected
+    bool removeEdge();          // function to remove edge 
+    bool removeVertex();          // function to remove edge 
+    void BFS(int s);    // prints BFS traversal from a given source s 
+}; 
 
 
 #endif //GRAPH_H
