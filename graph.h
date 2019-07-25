127844be582661fef392e7033b24289f961cd18a
TVasquez254 <tvasquez@austincc.edu>
2019-07-25 07:43:16 -0500

    added vertex count function and destructor

--- graph.h
+++ graph.h
@@ -25,7 +25,9 @@ private:
 
 public: 
     Graph(int size);                   // Constructor 
-    void addEdge(int v, int w, int weight=0);     // Add w to v’s list
+    ~Graph ();      //Destructor
+    bool addEdge(int v, int w, int weight=0);     // Add w to v’s list
+    bool addVertice(int);
     void BFS(int s);                // prints BFS traversal from a given source s
     void DFSUtil(int v, bool visited[]);
     void DFS(int v);                // DFS traversal of the vertices reachable from v   
