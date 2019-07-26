61138d09fdcc3b1595357b206b7eb7ed1e8405d9
TVasquez254 <tvasquez@austincc.edu>
2019-07-25 22:50:43 -0500

    delete additional checks

--- graph.cpp
+++ graph.cpp
@@ -66,31 +66,6 @@ bool Graph::addVertice(int v)
 
 }
 
-// int Graph::numVertex(int v)
-// {
-//     int count =0;
-//     for (int i = 0; i<size; i++)
-//     {
-//         if (vertices[v])
-//         {
-//             count++;
-//         }
-//     }
-//     return count;
-    
-// }
-
-
-// int Graph::numEdges()
-// {
-//     int count =0;
-//     for (int i=0; i<size; i++)
-//     {
-//         count += adj[i].size();
-//     }
-//     return count;
-
-// }
 
 bool Graph::addVertice(int v)
 {
