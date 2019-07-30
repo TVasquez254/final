365dfb799b8dc03645e7335174f43590ad110ac8
TVasquez254 <tvasquez@austincc.edu>
2019-07-30 14:23:15 -0500

    one return statement

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-/************************************************************
+/***********************************************************
 Name: Tomas Vasquez 
 Assignment: Final
 Purpose: This program opens 3 diff files based on user input from command line (conducts basic file validation checks), 
@@ -6,7 +6,7 @@ and conducts graph operations based on if they are weighted or not and writes or
 Breath First Search, Depth First Search, add a vertex, delete a vertex, conduct checks for duplicates(edges or vertex), check 
 to delete an existing node, and checks connectivity and provides a list of items not connected.
 Notes: Operations: Breath First Search, Depth First Search, add/ delete (vertex/edge) and write to file (example.txt).
-************************************************************/
+***********************************************************/
 
 #ifndef MAIN_H
 #define MAIN_H
